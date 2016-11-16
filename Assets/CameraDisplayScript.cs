f15e1fec3ff00db1dc254056ef245225645c4d67
Livi Erickson <livi@outlook.com>
2016-11-16 13:55:26 -0800

    Initial commit of camera culling demo

---
+++ Assets/CameraDisplayScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDisplayScript : MonoBehaviour {
+
+    bool redVisible = true;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            if (!redVisible){
+                Camera.main.cullingMask = 1 | 1 << 9;
+                redVisible = true;
+            }
+            else {
+                Camera.main.cullingMask = 1 | 1 << 8;
+                redVisible = false;
+            }
+        }
+	}
+}
